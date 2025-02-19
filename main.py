695a6a54e39517024087f7d22c77c41b570d1dba
Zac Scott <zac.scott11@gmail.com>
2025-02-19 16:07:23 +1300

    feat(uv): add typer boilerplate

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from gh-semantic-release!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/scottzach1/semantic_release/main.py
@@ -0,0 +1,17 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def deploy():
+    """
+    Trigger a semantic release with the click of a button
+
+    Set --verbose to enable detailed output.
+    """
+    typer.echo("Hello world!")
+
+
+if __name__ == "__main__":
+    app()
